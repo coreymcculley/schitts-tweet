0f9d9736da97ba844c7ff9df8f863a5d68129af1
ipavlunin <ipavlunin@gmail.com>
2021-01-24 15:57:59 -0600

    working on flask app

--- IgorP/app/app.py
+++ IgorP/app/app.py
@@ -18,4 +18,47 @@ from flask_table import Table, Col
 
 app = Flask(__name__)
 
-#
+# function to load models
+
+
+def models_load():
+    global model1
+    global tokens1
+    global model2
+    global tokens2
+
+    with open('file_path', 'rb') as f1:
+        tokens1 = pickle.load(f1)
+        print("Tokenizer1 Loaded")
+
+    model1 = load_model('file_path')
+    print("Model1 Loaded")
+
+    with open('file_path', 'rb') as f2:
+        tokens2 = pickle.load(f2)
+        print("Tokenizer1 Loaded")
+
+    model2 = load_model('file_path')
+    print("Model1 Loaded")
+
+
+# function to scrap Tweeter
+tweets = []
+
+
+def username_tweets(username, count):
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True)
+
+        tweets = tweepy.Cursor(api.user_timeline, id=username).items(count)
+
+        tweets_df = pd.DataFrame(tweets_list, columns=[
+                                 'Username', 'Datetime', 'Tweet Id', 'Text'])
+        html_tab = tweets_df.to_html()
+        return html_tab
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)
